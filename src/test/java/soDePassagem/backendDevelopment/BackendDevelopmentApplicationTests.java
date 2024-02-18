505fb8d8110698501c534ce54f1493779ece8939
Caio Nápoles <131042543+caioNapoles@users.noreply.github.com>
2024-02-17 22:40:57 -0300

    Add files via upload

---
+++ src/test/java/soDePassagem/backendDevelopment/BackendDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package soDePassagem.backendDevelopment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
